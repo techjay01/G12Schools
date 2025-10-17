4fc11cccc35ea9941c067bde5daf8204664a60a7
techjay01 <techjay2023@gmail.com>
2025-10-17 15:00:24 +0100

    login and header and sidebar features

--- frontend/js/sidebar.js
+++ frontend/js/sidebar.js
@@ -1,16 +1,21 @@
 const menuIcon = document.getElementById('menu-icon');
 const sidebar = document.getElementById('sidebar');
+const closeSidebar = document.querySelector('.close-sidebar');
 
 menuIcon.addEventListener('click', () => {
 sidebar.classList.toggle('active');
+closeSidebar.classList.toggle('active');
 });
 
-document.querySelector('.close-sidebar').addEventListener('click', () => {
+closeSidebar.addEventListener('click', () => {
     sidebar.classList.remove('active');
+    closeSidebar.classList.remove('active');
+
 });
 
 document.addEventListener('click', (e) => {
-    if (!sidebar.contains(e.target) && !menuIcon.contains(e.target)) {
+    if (!sidebar.contains(e.target) && !menuIcon.contains(e.target) && !closeSidebar.contains(e.target)) {
         sidebar.classList.remove('active');
+        closeSidebar.classList.remove('active');
     }
 });
